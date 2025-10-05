7a8e497b99d6d64ce3ba77afd2114e3217f3081b
k4y4 <k4y4no@gmail.com>
2025-10-05 21:25:39 +0200

    Home functionalities unitTest

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,25 +1,33 @@
+import { of } from 'rxjs';
 import { expect } from '@jest/globals';
-
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
-
+import { ApiService } from '../../_services/api/api.service';
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
-
+  const apiServiceMock = {
+    getAllItems: jest.fn(() => of([]))
+  }
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [HomeComponent],
+      providers: [
+        { provide: ApiService, useValue: apiServiceMock }
+      ]
     })
     .compileComponents();
-
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should render title', () => {
+      const fixture = TestBed.createComponent(HomeComponent);
+      fixture.detectChanges();
+      const compiled: HTMLElement = fixture.nativeElement;
+      expect(compiled.querySelector('p')?.textContent).toContain('home works!');
   });
 });
