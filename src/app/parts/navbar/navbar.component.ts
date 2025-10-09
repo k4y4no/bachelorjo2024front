b97bd1707dcc84fc6a39258b0a9b2ae257a9d76e
k4y4no <85989182+k4y4no@users.noreply.github.com>
2025-10-10 01:24:42 +0200

    Setup navbar v1.00 (#6)

---
+++ src/app/parts/navbar/navbar.component.ts
@@ -0,0 +1,74 @@
+import { Component, HostListener, inject, OnInit } from '@angular/core';
+import { Event, NavigationEnd, Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../_services/authService/auth.service';
+import { TitleCasePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [
+    RouterModule,
+    TitleCasePipe
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent implements OnInit {
+
+    router = inject(Router)
+    authService = inject(AuthService)
+    innerWidth: number = 0;
+    isMenuOpen:boolean = false;
+    logMenu: string = "login"
+    isAuthenticated: boolean | null = null;
+
+    constructor(){
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this.toggleMenu()
+        this.isAuthenticated = this.authService.isConnected() ? true:false
+        // this.isAdmin = this.authService.isAdmin(this.tokenService.getRoleToken(this.authService.getToken()))
+        this.logMenu = this.authService.isConnected() ? "logout":"login"
+        // if(this.isAuthenticated) {
+        //   this.profileOn = this.isAdmin ? "dash":"profile"
+        // } else {
+        //   this.profileOn = "register"
+        // }
+
+
+      }
+    })
+  }
+
+
+  ngOnInit() {
+    this.innerWidth = window.innerWidth;
+
+  }
+
+  toggleMenu(): void{
+    if(this.innerWidth > 400){
+      return
+    }
+    this.isMenuOpen = !this.isMenuOpen
+
+  }
+
+  loggingOut(){
+    
+    this.authService.logout().subscribe({
+      next: (response) => {
+        console.log('Logout successful', response)
+        this.router.navigate(['/login'])
+      }
+    });
+    
+  }
+  
+  @HostListener('window:resize', ['$event'])
+  onResize(event:Event) {
+    this.innerWidth = window.innerWidth;
+        if(this.innerWidth > 400){
+          this.isMenuOpen = false
+    }
+  }
+}
