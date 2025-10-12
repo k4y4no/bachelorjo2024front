edac942b808918b43d4550af74548751d8a587c0
k4y4 <k4y4no@gmail.com>
2025-10-12 08:54:32 +0200

    Setup dashboard admin v0.5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,19 @@ import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { authGuard } from './_guards/auth.guard';
 
 export const routes: Routes = [
     {  path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'profile', component: ProfileComponent },
-    { path: 'dashboard', component: DashboardComponent },
+    { 
+        path: 'profile', component: ProfileComponent,
+        canActivate: [authGuard]},
+    { 
+        path: 'dashboard', 
+        component: DashboardComponent,
+        canActivate: [authGuard]
+    },
 ];
